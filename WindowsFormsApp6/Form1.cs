48cb8f94d6b2a194ea1cd172c66090c131f799b1
John Speed <rc0029514@student.riversidecollege.ac.uk>
2019-06-12 12:03:46 +0100

    hhh

--- WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/Form1.cs
@@ -31,18 +31,23 @@ namespace WindowsFormsApp6
         {
             try
             {
+                // i put the try catch in for an error message to be displayed when a letter is put into a resistor value box 
+                // i used double in order to set the value of each text box
                 double SR1 = double.Parse(textBox2.Text);
                 double SR2 = double.Parse(textBox3.Text);
                 double SR3 = double.Parse(textBox4.Text);
                 double SR4 = double.Parse(textBox5.Text);
                 double SR5 = double.Parse(textBox6.Text);
 
+                // below shows the formula i used in order for the series calculation to be complete 
                 double TotalResistance = SR1 + SR2 + SR3 + SR4 + SR5;
+                // below is the code i used for the total resistance to show up afte it has been calculated 
                 MessageBox.Show("TotalResistance = " + TotalResistance.ToString("0.00") + "Ω");
             }
 
             catch
             {
+                // below is the message that would show up for the error message
                 MessageBox.Show("Error in inputting values.");
             }
         }
@@ -51,13 +56,17 @@ namespace WindowsFormsApp6
         {
             try
             {
+                //i used the try catch method once again to display an error message if a letter was put in a ressistor value box 
+                // below here, i used double to assign each text box a resistor value
                 double PR1 = double.Parse(textBox7.Text);
                 double PR2 = double.Parse(textBox8.Text);
                 double PR3 = double.Parse(textBox9.Text);
                 double PR4 = double.Parse(textBox10.Text);
                 double PR5 = double.Parse(textBox1.Text);
 
+                //below is the formula i used in order for this calculation to be complete. it is similar to the series calculation
                 double TotalResistance = (1.0 / (1.0 / PR1 + 1.0 / PR2 + 1.0 / PR3 + 1.0 / PR4 + 1.0 / PR5));
+                // the code below will display the answer for an calculation 
                 MessageBox.Show("TotalResistance = " + TotalResistance.ToString("0.00") + "Ω");
             }
 
